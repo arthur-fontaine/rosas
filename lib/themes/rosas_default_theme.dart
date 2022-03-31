288ae96aa5b5e0be153dccdc47852d9baed7bb42
Arthur Fontaine <0arthur.fontaine@gmail.com>
2022-03-31 22:39:23 +0200

    feat: add a text theme

--- lib/themes/rosas_default_theme.dart
+++ lib/themes/rosas_default_theme.dart
@@ -14,4 +14,41 @@ final rosasDefaultTheme = ThemeData(
     onSurface: Color.fromRGBO(0, 0, 34, 1),
     onError: Color.fromRGBO(255, 255, 255, 1),
   ),
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+      fontFamily: 'Archivo',
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w600,
+      fontSize: 22,
+      height: 1,
+    ),
+    headline2: TextStyle(
+      fontFamily: 'Archivo',
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+      height: 1.3,
+    ),
+    bodyText1: TextStyle(
+      fontFamily: 'Archivo',
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      fontSize: 16,
+      height: 1.3,
+    ),
+    caption: TextStyle(
+      fontFamily: 'Archivo',
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w100,
+      fontSize: 14,
+      height: 1.3,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: 'Archivo',
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      fontSize: 12,
+      height: 1,
+    ),
+  )
 );
